6d297de45b6a41cdabd416a3d9a0b82abb76c56c
cimaron713 <cimaron713@gmail.com>
2024-05-09 15:00:15 +0900

    fix: 테스트 제거

--- src/test/java/com/capstone/reviewsummary/CwalTest.java
+++ src/test/java/com/capstone/reviewsummary/CwalTest.java
@@ -9,14 +9,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class CwalTest {
     @Autowired
     private CrawlingService crawlingService;
-
-    @Test
-    void test() throws Exception{
-        crawlingService.crawlCoupangReview("https://www.coupang.com/vp/product/reviews?productId=7694805260&page=1&size=30&sortBy=DATE_DESC");
-    }
-    @Test
-    void test2() throws Exception{
-        crawlingService.cookieCheck("https://www.coupang.com/vp/product/reviews?productId=7694805260&page=1&size=30&sortBy=DATE_DESC");
-        System.out.println("CwalTest.test");
-    }
 }
